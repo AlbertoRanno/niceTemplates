080a92fc8067627db0f196606769367e2ae26c33
AlbertoRanno <albertoranno@hotmail.com>
2023-07-24 09:53:32 -0300

    Subo back-up de Templates Actuales de Nice

---
+++ listaDesplegable.js
@@ -0,0 +1,2 @@
+$(function() { $('.EXPAND_COLLAPSE').find('dt').on('click', function() { $(this).next().toggle('350'); }); });
+
---
+++ tabla.js
@@ -0,0 +1,1 @@
+$(function() { $('.EXPAND_COLLAPSE').find('dt').on('click', function() { $(this).next().toggle('350'); }); });
