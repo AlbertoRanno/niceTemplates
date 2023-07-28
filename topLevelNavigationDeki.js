586bd0a5b582d243ca1f807cdbc78cbc9e5492fa
AlbertoRanno <albertoranno@hotmail.com>
2023-07-28 12:30:47 -0300

    instale ext para Deki (liquid), subo back-up de las variables en Deki, una version de la navBar html corregida a JS integral, y otra en deki corregido

---
+++ topLevelNavigationDeki.js
@@ -0,0 +1,13 @@
+function alternarCollapse(id) {
+    const elemento = document.getElementById(id);
+  
+    if (elemento) {
+      elemento.classList.toggle('active');
+    }
+  }
+  
+  $(function() {
+    $('.EXPAND_COLLAPSE').find('dt').on('click', function() {
+        $(this).next().toggle('350');   
+     });
+  });
