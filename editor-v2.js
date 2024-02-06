6352b59ec5c702e7086de9d42ef4c0747128c557
AlbertoRanno <albertoranno@hotmail.com>
2024-02-06 10:52:13 -0300

    scripts tooltips - css roles - cambios de settings y filtros - diagramas (powerPoint a html)

--- editor-v2.js
+++ editor-v2.js
@@ -10,15 +10,17 @@ CKEDITOR.editorConfig = function(config) {
     config.stylesSet.push({ name : 'hasContent', wrap:false, attributes : { 'class' : 'hasContent'} });
     config.stylesSet.push({ name : 'emptyRegions', wrap:false, attributes : { 'class' : 'emptyRegions'} });
     // config.stylesSet.push({ name : 'tooltips', wrap: false, attributes: { 'class': 'tool-tip-link', 'title': '{{reemplazarAqui}}'}});
-    
+    /*        
     config.stylesSet.push({ name : 'tooltips_CAT-CAD', wrap: false, attributes: { 'class': 'tool-tip-link CAT-CAD'}});
     config.stylesSet.push({ name : 'tooltips_CGD', wrap: false, attributes: { 'class': 'tool-tip-link CGD'}});
     config.stylesSet.push({ name : 'tooltips_CAP', wrap: false, attributes: { 'class': 'tool-tip-link CAP'}});
     config.stylesSet.push({ name : 'tooltips_todosLosPuestos', wrap: false, attributes: { 'class': 'tool-tip-link todosLosPuestos'}});
+    config.stylesSet.push({ name : 'tooltips_todosAsesores', wrap: false, attributes: { 'class': 'tool-tip-link todosAsesores'}});
     config.stylesSet.push({ name : 'tooltips_CAT-CGD', wrap: false, attributes: { 'class': 'tool-tip-link CAT-CGD'}});
     config.stylesSet.push({ name : 'tooltips_CGD-CAP', wrap: false, attributes: { 'class': 'tool-tip-link CGD-CAP'}});
     config.stylesSet.push({ name : 'tooltips_CAT-CAP', wrap: false, attributes: { 'class': 'tool-tip-link CAT-CAP'}});
-
+    config.stylesSet.push({ name : 'tooltips_todosAsesoresCGD-CAP', wrap: false, attributes: { 'class': 'tool-tip-link todosAsesoresCGD-CAP'}});
+    */
     config.stylesSet.push({ name : '2 Columnas', element : 'div', wrap:true, attributes : { 'class' : 'generateColumns twoColumns' } });
     config.stylesSet.push({ name : '3 Columnas', element : 'div', wrap:true, attributes : { 'class' : 'generateColumns threeColumns' } });
     config.stylesSet.push({ name : '4 Columnas', element : 'div', wrap:true, attributes : { 'class' : 'generateColumns fourColumns' } });
